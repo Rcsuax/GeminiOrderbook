29a39cca8a9504b7c7dacf5e8ab138ae4f59c778
Reuben S <rcsuax@gmail.com>
2022-03-31 10:56:51 +0100

    minor refactor + touch

--- orderbook.py
+++ orderbook.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 import ssl
+import enum
 import json
 import websocket
 import threading
+from decimal import Decimal
 from queue import Queue
 from dataclasses import dataclass
 from collections import defaultdict
 
-
-class OrderSource:
-    def __init__(self, queue):
+class DataSource:
+    def __init__(self, queue: Queue):
         """
-        Interface with the Gemini Websockets API to
-        receive live order book data then submits it to a queue
-        to be processed by the main thread.
+        Data Source for MARKET DATA Gemini Websockets API, primary job is to populate a queue of orders
         """
         self.queue = queue
         self.ws = websocket.WebSocketApp(
@@ -23,43 +22,53 @@ class OrderSource:
         )
 
     def start(self):
+        """ 
+        Start the websocket connection 
+        """
         self.ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
 
     def on_open(self, _):
         print("====== CONNECTED TO GEMINI ======")
 
     def on_message(self, _, message):
+        """
+        The initial response message will show the existing state of the order book. 
+        Subsequent messages will show all executed trades, orders placed or canceled.
+        """
         msg = json.loads(message)
         events = msg["events"]
 
         for event in events:
             if event:
-                px = float(event["price"])
-                qty = float(event["remaining"])
+                px = Decimal(event["price"])
+                qty = Decimal(event["remaining"])
                 side = event["side"]
                 reason = event["reason"]
                 self.queue.put(Order(px, qty, side, reason))
 
 
+
+class Side(enum.Enum):
+    BUY = "buy"
+    SELL = "sell"
+
 @dataclass
 class Order:
     price: float
     remaining: float
-    side: str
+    side: Side
     reason: str
 
     def __repr__(self):
-        return f"{self.price} x {self.remaining}"
+        return f"{self.price} {self.remaining}"
+
 
 
 class OrderBook:
     def __init__(self):
         """
-        initialize the order book with a defaultdict of deques
+        initialize the order book with a defaultdict of lists
         where the index is the price and the value is a list of orders
-        expect O(1) lookup time and O(1) insertion time
-        expect O(n) lookup time for worst case
-
         self.bids = { (price): [order1, order2, ...] }
         self.asks = { (price): [order1, order2, ...] }
         """
@@ -68,8 +77,18 @@ class OrderBook:
         self.bid_head = None
         self.ask_head = None
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"============ BOOK STATE ============\nBids: {len(self.bids)}\nAsks: {len(self.asks)}"
+    
+    def __len__(self) -> int:
+        return len(self.bids) + len(self.asks)
+
+    def print_summary(self):
+        print(self)
+        self.print()
+
+    def print(self):
+        print(f"{self.bid_head}\t{self.ask_head}")
 
     def process_order(self, order: Order):
         if order.remaining == 0:
@@ -87,17 +106,17 @@ class OrderBook:
 
         else:
             if order.reason == "cancel":
-                # delete order from price level
+                # delete the order from price level
                 if order.side == "bid":
                     # list comprehesnsion removes the matched order from the list
                     self.bids[order.price] = [o for o in self.bids[order.price] if o.remaining != order.remaining ]
                     
-                    if self.bid_head.price == order.price and self.bid_head.remaining == order.remaining:
+                    if len(self.bids[order.price]) == 0 and self.bid_head.price == order.price and self.bid_head.remaining == order.remaining:
                         self.bid_head = None
                 else:
                     self.asks[order.price] = [o for o in self.asks[order.price] if o.remaining != order.remaining ]
 
-                    if self.ask_head.price == order.price and self.ask_head.remaining == order.remaining:
+                    if len(self.asks[order.price]) == 0 and self.ask_head.price == order.price and self.ask_head.remaining == order.remaining:
                         self.ask_head = None
             elif order.reason == "initial" or order.reason == "place":
                 # if price level doesnt exist, defaultdict will create it for us
@@ -121,9 +140,6 @@ class OrderBook:
                     # add order to asks
                     self.asks[order.price].append(order)
 
-    def print_summary(self):
-        print(self)
-        print(f"{self.bid_head}\t{self.ask_head}")
 
 
 def run(queue: Queue, book: OrderBook):
@@ -139,11 +155,11 @@ def run(queue: Queue, book: OrderBook):
 
 
 if __name__ == "__main__":
-    order_pool = Queue(maxsize=0) # FIFO Queue or PRIORITY Queue
+    order_pool = Queue(maxsize=0) # FIFO Queue
     order_book = OrderBook()
-    order_source = OrderSource(queue=order_pool)
+    data_source = DataSource(queue=order_pool)
 
-    thread_data_stream = threading.Thread(target=order_source.start, daemon=True)
+    thread_data_stream = threading.Thread(target=data_source.start, daemon=True)
     thread_output = threading.Thread(target=run, args=(order_pool, order_book))
 
     thread_data_stream.start()
